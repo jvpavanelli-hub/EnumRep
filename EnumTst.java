4047f153854889e8583ad325d51bc8d36a378d59
jk zago <joaovpavanelli@gmail.com>
2026-04-23 22:35:50 -0300

    Pesquisa Enum Joao Vitor

---
+++ EnumTst.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package enumtst;
+
+/**
+ *
+ * @author Admin
+ */
+public class EnumTst {
+
+
+    public static void main(String[] args) {
+        Tamanho minhaCamiseta = Tamanho.MEDIO;
+
+        
+        System.out.println("O tamanho escolhido foi: " + minhaCamiseta);
+    }
+ }
+    
+
